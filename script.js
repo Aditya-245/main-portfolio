3767efdc146866d07b78eae36b269008addb5fda
Aditya-245 <adityashiralkar9@gmail.com>
2025-08-31 16:05:14 -0700

    final mobile responsive

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ body.addEventListener("mousemove",function(det){
     curs.style.top = det.clientY + "px";
 
 })
-var button=document.querySelector("#feedback button")
-button.addEventListener("click", function(){
-       var feedback = document.querySelector("#feedback textarea").value;
-       
-}
-)
+body.addEventListener("focusin",function(det){
+    curs.style.left = det.clientX + "px";
+    curs.style.top = det.clientY + "px";
+
+})
